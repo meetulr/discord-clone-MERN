d300b0a4158e5057f7584d39f9c932df3992cce9
meetul <meetulrathore98@gmail.com>
2023-10-24 16:49:39 +0530

    add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
 const font = Open_Sans({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Team Chat Application",
@@ -16,8 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className,
+         "bg-white dark:bg-[#313338]" 
+          )}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+            >
+            {children}
+</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
