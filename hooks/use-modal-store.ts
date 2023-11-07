22f2fb839a9ab3d446d1cdc0c5296ac627dde295
meetul <meetulrathore98@gmail.com>
2023-11-07 13:20:40 +0530

    add delete message functionality

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -3,7 +3,7 @@ import { create } from "zustand";
 
 export type ModalType = "createServer" | "invite" | "editServer"
   | "members" | "createChannel" | "leaveServer" | "deleteServer"
-  | "editChannel" | "deleteChannel" | "messageFile";
+  | "editChannel" | "deleteChannel" | "messageFile" | "deleteMessage";
 
 interface ModalData {
   server?: ServerObject;
